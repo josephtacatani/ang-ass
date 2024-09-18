63fedf1819638e4216c4990dc121019f5a1cfb4a
ニール <neiljosephtacatani@Neil.local>
2024-09-18 23:16:16 +0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactCardComponent } from './contact-card/contact-card.component';
+import { ContactTableComponent } from './contact-table/contact-table.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactViewComponent } from './contact-view/contact-view.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactListComponent,
+    ContactCardComponent,
+    ContactTableComponent,
+    ContactFormComponent,
+    ContactViewComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
